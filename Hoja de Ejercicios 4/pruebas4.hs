0388ffe138de6a894a858a61d6a6129b21020647
Victor Manuel Cavero Gracia <victorgracia7@gmail.com>
2019-10-28 20:38:54 +0100

    Update pruebas4.hs

--- Hoja de Ejercicios 4/pruebas4.hs
+++ Hoja de Ejercicios 4/pruebas4.hs
@@ -1,11 +1,19 @@
-maximum' :: (Num a, Ord a) => [a] -> a
-maximum' [] = error "Empty List"
-maximum' (x:[]) = x
-maximum' (x:xs) = if (x < maximum' xs) then maximum' xs else x
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
 
 
 data Pila = P [Integer] deriving (Show)
 
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (x:xs) = f x : map' f xs
+ 
+
+flip' :: (a -> b -> c) -> (b -> a -> c)
+flip' f x y = f y x
 
 sumame4 x = if (x < 100) then x + 4 else x
 sumame3 x = x + 3
