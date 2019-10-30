3a4f0c037d7299592e82dd542138f1cc7d8f86bf
Victor Manuel Cavero Gracia <victorgracia7@gmail.com>
2019-10-30 19:53:48 +0100

    Made 1 and 2 exercises

--- Hojas de Ejercicios/Hoja de Ejercicios 4/pruebas4.hs
+++ Hojas de Ejercicios/Hoja de Ejercicios 4/pruebas4.hs
@@ -1,24 +1,61 @@
+import Data.List
+import Data.Char
+{-
+    EJERCICIO 1
+-}
+data Pila a = P [a] deriving (Show)
 
-zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
-zipWith' _ [] _ = []
-zipWith' _ _ [] = []
-zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+creaPila :: Pila a
+creaPila = P []
+
+esPilaVacia :: Pila a -> Bool
+esPilaVacia (P xs)
+    | null xs = True
+    | otherwise = False
+
+apilar :: a -> Pila a -> Pila a --ojo te devuelve una pila nueva
+apilar x (P xs) = P $ x : xs
+
+cima :: Pila a -> a
+cima (P []) = error "Pila vacia"
+cima (P xs) = head xs
+
+desapilar :: Pila a -> Pila a
+desapilar (P []) = P []
+desapilar (P xs) = P $ tail xs 
+
+{- Que significa esta fun: 
+
+Va apilando la lista siguiendo el propio orden de la lista, de manera que como
+en una pila los elementos siempre se ponen en la cima esta queda ordenada de forma inversa.
+
+Como elemento neutro tiene la pila vacía y la lambda que va apilando.
+
+-}
+
+r :: [a] -> [a]
+r xs = ys 
+    where P ys = foldl (\p x -> apilar x p) creaPila xs
+
+{-
+    EJERCICIO 2
+
+    Data Maybe a = Nothing | Just a
+-}
+
+primeroQueCumple :: (a -> Bool) -> [a] -> Maybe a
+primeroQueCumple f [] = Nothing 
+primeroQueCumple f (x:xs) 
+    | f x = Just x
+    | otherwise = primeroQueCumple f xs
+
+{-
+    EJERCICIO 3, ES MUY PARECICDO AL 1
+-}
+
+{-
+    EJERCICIO 4
+-}
 
 
-data Pila = P [Integer] deriving (Show)
 
-map' :: (a -> b) -> [a] -> [b]
-map' _ [] = []
-map' f (x:xs) = f x : map' f xs
- 
-
-flip' :: (a -> b -> c) -> (b -> a -> c)
-flip' f x y = f y x
-
-sumame4 x = if (x < 100) then x + 4 else x
-sumame3 x = x + 3
---esPilaVacia
-
---apilar
-
---desapilar
