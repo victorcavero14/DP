a36fb6084fa40163035219ecda6f60461f6172fe
Victor Manuel Cavero Gracia <victorgracia7@gmail.com>
2019-11-23 18:19:44 +0100

    Added lab5

--- Laboratorios/lab5.hs
+++ Laboratorios/lab5.hs
@@ -2,7 +2,46 @@
 
 import Data.List
 import Data.Char
+import Data.List.Split
+import System.IO
 
+--Ejercicio 1: 
 
+adivina :: Int -> IO()
+adivina n = do
+    putStrLn "Guess: "
+    number <- getLine
+    if ((read number :: Int) == n) then putStrLn "Congrats!!! You guessed!"
+    else
+        if ((read number :: Int) > n) then do
+            putStrLn "Oh... that's higher"
+            adivina n
+        else do 
+            putStrLn "Oh... that's lower"
+            adivina n
 
---MIRARSE EL HELLO WORLD.hs :)
+--Ejercicio 2:
+
+contarpalabras :: IO() 
+contarpalabras = do
+    putStrLn "Insert a line:" 
+    line <- getLine
+    let {x = length (wordsBy (== ' ') line)} in putStrLn ("Size: " ++ (show x))
+
+--Ejercicio 3:
+
+--a:
+palabras:: String -> IO () 
+palabras path = do
+    handle <- openFile path ReadMode
+    contents <- hGetContents handle
+    (let {x = length (wordsBy (== ' ') contents)} in putStrLn ("Size: " ++ (show x)) )
+    hClose handle
+
+--b
+
+--c
+
+--d
+
+--e
