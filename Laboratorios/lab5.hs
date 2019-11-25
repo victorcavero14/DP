e2bba88ec366a6cddd581de6c9e12432e7d141eb
Victor Manuel Cavero Gracia <victorgracia7@gmail.com>
2019-11-25 19:03:05 +0100

    Working in Lab5

--- Laboratorios/lab5.hs
+++ Laboratorios/lab5.hs
@@ -1,4 +1,5 @@
--- Alumno: Víctor Manuel Cavero Gracia
+-- Alumno: Víctor Manuel Cavero Gracia 3ºA
+-- Asignatura: Programación Declarativa
 
 import Data.List
 import Data.Char
@@ -9,7 +10,7 @@ import System.IO
 
 adivina :: Int -> IO()
 adivina n = do
-    putStrLn "Guess: "
+    putStr "Guess: "
     number <- getLine
     if ((read number :: Int) == n) then putStrLn "Congrats!!! You guessed!"
     else
@@ -24,24 +25,63 @@ adivina n = do
 
 contarpalabras :: IO() 
 contarpalabras = do
-    putStrLn "Insert a line:" 
+    putStr "Insert a line: " 
     line <- getLine
-    let {x = length (wordsBy (== ' ') line)} in putStrLn ("Size: " ++ (show x))
+    let {x = length (words line)} in putStrLn ("Size: " ++ (show x))
 
 --Ejercicio 3:
 
 --a:
-palabras:: String -> IO () 
+
+palabras:: String -> IO Int 
 palabras path = do
     handle <- openFile path ReadMode
     contents <- hGetContents handle
-    (let {x = length (wordsBy (== ' ') contents)} in putStrLn ("Size: " ++ (show x)) )
+    let x = length (words contents)
+    putStrLn ("Size: " ++ (show x))
     hClose handle
+    return x
 
 --b
 
+palabras' :: IO()
+palabras' = do
+    putStr "Insert a line: " 
+    line <- getLine
+    handle <- openFile line ReadMode
+    contents <- hGetContents handle
+    let x = length (words contents)
+    putStrLn ("El fichero " ++ line ++ " tiene " ++ (show x) ++ " palabras.")
+    hClose handle
+
+
 --c
 
+promedia:: IO()
+promedia = do 
+    putStr "Insert a number: "
+    line <- getLine
+    let num = (read line :: Int)
+    promedia_aux num num 1
+    
+promedia_aux:: Int -> Int -> Int -> IO()
+promedia_aux num suma n = do
+    if (num /= -1 && num >= 0) then do
+        putStrLn ("Sum: " ++ show suma)
+        putStrLn ("Average: " ++ (show (div suma n)))
+        putStr "Insert a number: "
+        line <- getLine
+        let numero = (read line :: Int)
+        promedia_aux numero (suma+numero) (n+1)
+    else return ()
+    
+    
 --d
 
+--formatea:: String -> String -> Int -> IO()
+--formatea fileIn fileOut n =
+
 --e
+
+--calculadora :: IO()
+--calculadora =
